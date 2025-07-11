67823025043fa8cf283751d12ab3476e0ce12a8d
Ronaldo Cesar <156006721+odlaanoR@users.noreply.github.com>
2025-07-10 21:06:37 -0300

    Update underGararem001.py

--- PCS/underGararem001.py
+++ PCS/underGararem001.py
@@ -6,7 +6,7 @@ import time
 pygame.init()
 largura = 640
 altura = 480
-velocidade = 7
+velocidade = 3.2
 x = 65
 y = 450
 
@@ -19,8 +19,8 @@ fonteBatalha = pygame.font.SysFont('comicsans', 20, True, False)
 pygame.display.set_caption('Poggers')
 icon = pygame.image.load('assets/images.png')
 pygame.display.set_icon(icon)
-musicaFundo = pygame.mixer.music.load('assets/megalovania.mp3')
-pygame.mixer.music.set_volume(0.6)
+musicaFundo = pygame.mixer.music.load('assets/Project147.mp3')
+pygame.mixer.music.set_volume(0.45)
 pygame.mixer.music.play(-1)
 
 class Janelas():
@@ -42,37 +42,104 @@ class Janelas():
         
     def corFundo(self,cor=(0,0,0)):
         self.tela.fill(cor)
-
-class Acoes():
+        
+    def desenhaCaixa(self, caixa):
+        self.caixa = pygame.draw.rect(janela.tela, (255,255,255), caixa, width=7)
+    
+class Acoes(): #to começando a achar que essa classe ta maior doq deveria mas fazer oq
     corPadrao = (150,75,0)
     novaCor = (255,255,0)
     
-    def __init__(self, pos_x, msg):
+    def __init__(self, pos_x, msg): 
         self.botao = pygame.Rect(pos_x, 430, 100, 40)
         self.cor = self.corPadrao
         self.mostraMsg = False
         self.mensagem = msg
+        self.passou = False
+        self.gambiarraMsg = pygame.Rect((0,0,0,0))
+        self.mira = pygame.Rect((0,0,0,0))
+        self.x_mira = 40
+        self.mirando = False
+        self.comecaBatalha = False #isso indica se a batalha deve ou não começar
+        self.naBatalha = False #e esse aqui impede que o x e o y do jogador seja resetado INFINITAMENTE PQP EU N AGUENTO MAIS (não pensei em um nome melhor)
+        self.comecoMira = 0 #tive que criar isso aqui pq a mira tava selecionando automaticamente
         
-    def desenhaAtos(self):
+    def desenhaAcoes(self):
         pygame.draw.rect(janela.tela, self.cor, self.botao, width=2)
 
-    def checaAlma(self, player):
-        if player.colliderect(self.botao):
+    def checaAlma(self):
+        if alma.rect.colliderect(self.botao):
             self.cor = self.novaCor
+            if self.passou:
+                passaAcao = pygame.mixer.Sound('assets/sounds/snd_squeak.wav')
+                passaAcao.set_volume(0.3)
+                passaAcao.play()
+                self.passou = False   
         else:
             self.cor = self.corPadrao
+            self.passou = True
             
-    def clique(self,player, tecla):
-        if tecla == K_z and player.colliderect(self.botao):
-            self.mostraMsg = True
+    def checaClique(self, tecla):
+        if tecla == K_z and alma.rect.colliderect(self.botao):
+            self.mostraMsg = True  
+            clicaAcao = pygame.mixer.Sound('assets/sounds/snd_select.wav')
+            clicaAcao.set_volume(0.4)
+            clicaAcao.play()
+            self.gambiarraMsg = pygame.Rect(30, 215, 10, 10)
             
-    def cancelaAto(self):
+    def cancelaAcao(self):
         self.mostraMsg = False
             
-    def atoMensagem(self, fonte):
+    def acoesMensagem(self, fonte):
         if self.mostraMsg:
             janela.tela.blit(fonte.render(self.mensagem, True, (255,255,255)), (43,205))
+
+    def confirmaAcao(self, tecla):
+        if self.mostraMsg and tecla == K_z and alma.rect.colliderect(self.gambiarraMsg):
+            clicaAcao = pygame.mixer.Sound('assets/sounds/snd_select.wav')
+            clicaAcao.set_volume(0.4)
+            clicaAcao.play()
+            self.mostraMsg = False
+            return True
+        return False
+    
+    def confirmaLuta(self):
+        self.mirando = True
+        self.comecoMira = pygame.time.get_ticks()
+        
+    def confirmaAgir(self):
+        print('poggers')
+        
+    def confirmaItem(self):
+        print('comeu')
+        
+    def confirmaPiedade(self):
+        print('Mas não estava amarelo')
+        
+    def mirar(self, tecla):
+        if self.mirando:
+            pygame.draw.rect(janela.tela, (0, 0, 0), self.gambiarraMsg)
+            self.alvo = pygame.image.load('assets/mira.png')
+            self.alvo = pygame.transform.scale(self.alvo, (600, 135))
+            janela.tela.blit(self.alvo, (20, 205))
+            self.x_mira += 5.6
+            self.mira = pygame.Rect(self.x_mira, 205, 7, 140)
+            pygame.draw.rect(janela.tela, (255,255,255), self.mira)
+            tempoAtual = pygame.time.get_ticks()
+            if self.x_mira >= 620 or (tecla == K_z and tempoAtual - self.comecoMira >= 100):
+                self.mirando = False
+                self.x_mira = 40
+                self.comecaBatalha = True
             
+    def batalhaAcontece(self):
+        global x, y, tempoLuta
+        if self.comecaBatalha and self.naBatalha == False:
+            tempoLuta = pygame.time.get_ticks()
+            janela.mudarTela('lutaAcontecendo')
+            x = 320
+            y = 260
+            self.naBatalha = True
+        
 class Ataque():
     def __init__(self, cor , ataque_x, ataque_y, ataque_w, ataque_h, mov_x, mov_y, vel):
         self.retangulo = pygame.Rect(ataque_x, ataque_y, ataque_w, ataque_h)
@@ -81,13 +148,15 @@ class Ataque():
         self.vel = vel
         self.cor = cor
         self.mostrar = True
+        self.contador = 0 #opa coisa nova, não sei se vai ficar aqui por mt tempo, mas nesse ataque padrão vai servir (explico lá embaixo)
 
     def atualizar(self, alma_rect):
         if self.mostrar == True:
             self.retangulo.x += self.mov_x
             self.retangulo.y += self.mov_y
-            if self.retangulo.x >= 640:
-                self.retangulo.x = 0
+            if self.retangulo.x >= 530:
+                self.retangulo.x = 80
+                self.contador += 1
             if self.retangulo.colliderect(alma_rect):
                 print("colidiu!")
                 self.mostrar = False
@@ -95,17 +164,16 @@ class Ataque():
             return False
             
     def draw(self):
-        if self.mostrar == True:
+        if self.mostrar:
             self.sla1 = pygame.draw.rect(janela.tela, self.cor, self.retangulo)
- 
+    
 class Alma(pygame.sprite.Sprite):
-
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.sprites = []
-        self.sprites.append(pygame.image.load('assets/alma.png'))
-        self.sprites.append(pygame.image.load('assets/almaazul.png'))
-        self.sprites.append(pygame.image.load('assets/almaverde.png'))       
+        self.sprites.append(pygame.image.load('assets/sprites/alma.png'))
+        self.sprites.append(pygame.image.load('assets/sprites/almaazul.png'))
+        self.sprites.append(pygame.image.load('assets/sprites/almaverde.png'))       
         self.estado = 0
         self.image = self.sprites[self.estado]
         self.image = pygame.transform.scale(self.image, (16, 16))
@@ -126,105 +194,120 @@ botoes = [
     ]
 
 alma = Alma()
-ataque = Ataque((255,255,255), 0, 110, 20, 20, 3, 0, 5)
+ataque = Ataque((255,255,255), 80, 220, 20, 20, 3, 0, 5)
+ataque2 = Ataque('white', 90, 250, 40, 40, 10, 0, 5)
+ataque3 = Ataque('red', 150, 150, 30, 30, 0, 0, 0)
 artes = pygame.sprite.Group()
 artes.add(alma)
 
 def reiniciar_jogo():
-    global vidaAtual, gameover, x, y, musicaFundo, bateu
+    global vidaAtual, gameover, x, y, musicaFundo
     vidaAtual = 92
     x = 65
     y = 445
     gameover = False
-    musicaFundo = pygame.mixer.music.load('assets/megalovania.mp3')
-    pygame.mixer.music.set_volume(0.6)
+    musicaFundo = pygame.mixer.music.load('assets/Project147.mp3')
+    pygame.mixer.music.set_volume(0.45)
     pygame.mixer.music.play(-1)
     janela.mudarTela('ações')
-    
     
 while True:
     fps.tick((60))
     pygame.display.update()
-    for evento in pygame.event.get():
-        if evento.type == KEYDOWN:
-            
+    eventos = pygame.event.get()
+    confirmaAtaque = None
+    for evento in eventos:
+        if evento.type == KEYDOWN and janela.telaAtual == 'ações':
+            confirmaAtaque = evento.key
             if evento.key == K_RIGHT and not alma.rect.colliderect(botoes[3].botao) and not any(botao.mostraMsg for botao in botoes):
                 x += 140
             if evento.key == K_LEFT and not alma.rect.colliderect(botoes[0].botao) and not any(botao.mostraMsg for botao in botoes):
                 x -= 140
-                            
+        
             if evento.key == K_z:
                 for botao in botoes:
-                    botao.clique(alma.rect, evento.key)
+                    botao.checaClique(evento.key)
+                if botoes[1].confirmaAcao(evento.key):
+                    botoes[1].confirmaAgir()
+                if botoes[0].confirmaAcao(evento.key):
+                    botoes[0].confirmaLuta()
+                    botoes[0].batalhaAcontece()
+                if botoes[2].confirmaAcao(evento.key):
+                    botoes[2].confirmaItem()
+                if botoes[3].confirmaAcao(evento.key):
+                    botoes[3].confirmaPiedade()
                 x = 30
                 y = 220
-       
-            if evento.key == K_x:
+
+            if evento.key == K_x and not any(botao.mirando for botao in botoes):
                 for botao in botoes:
-                    botao.cancelaAto()
+                    botao.cancelaAcao()
                 x = 65
                 y = 450
-                janela.mudarTela('ações')
-                janela.atualizaTela()
             
-            if evento.key == K_F11:
-                #print("Pondo em tela cheia...")
-                pygame.display.toggle_fullscreen()
-            elif evento.key == K_j:
-                print('mudando o estado da alma para ', {alma.estado})
-                alma.estado += 1
-                if alma.estado == 3:
-                    alma.estado = 0
         if evento.type == QUIT:
             pygame.quit()
             exit()
-            
+   
     janela.corFundo()
-    caixa = pygame.draw.rect(janela.tela, (255,255,255),(10, 190, 620, 180), width=7)
     janela.escreveTexto(f'HP:{vidaAtual}/{vida}', fonte, (255,255,255),(260,385))
+    if janela.telaAtual == 'lutaAcontecendo':
+        janela.desenhaCaixa((70, 190, 500, 180))
+    else:
+        janela.desenhaCaixa((10,180,620,180))
         
+    almaDesaparece = any(botao.mirando for botao in botoes) 
+
+    if almaDesaparece == False:
+        janela.tela.blit(alma.image, alma.rect)
+    alma.update()
+            
     for botao in botoes:
-        botao.checaAlma(alma.rect)
-        botao.desenhaAtos()
-        botao.atoMensagem(fonteBatalha)
-        
-    if ataque.mostrar:
-        ataque.draw()      
-        if ataque.atualizar(alma.rect) and ataque.mostrar == False:
-            vidaAtual -= 1
-        
-   #desconsidere isso no jogo normal, é apenas pra testar o ataque lá 
-    teclas = pygame.key.get_pressed() 
-    if teclas[pygame.K_w]:
-        y -= 7
-        
-    if teclas[pygame.K_s]:
-        y += 7
-        
-    if teclas[pygame.K_d]:
-        x += 7
-        
-    if teclas[pygame.K_a]:
-        x -= 7
-        
-    #Esse aqui botei nos comentários pq fico testando a tela de gameover.
+        botao.checaAlma()
+        botao.desenhaAcoes()
+        botao.acoesMensagem(fonteBatalha)
+        botao.mirar(confirmaAtaque)
+        botao.batalhaAcontece()
+    confirmaAtaque = None
+ 
+    #Esses aqui botei nos comentários pq fico testando a tela de gameover.
+    #teclas = pygame.key.get_pressed() 
+    #if teclas[pygame.K_w]:
+        #y -= 7  
     #if alma.rect.colliderect(caixa):
         #vidaAtual -= 10
 
-    if alma.estado == 1:
-        y += 0
-        pygame.display.set_caption("Você está azul agora!")
+    if janela.telaAtual == 'lutaAcontecendo':
+        teclas = pygame.key.get_pressed()
+        if teclas[pygame.K_x]:
+            velocidade = 2.2
+        else:
+            velocidade = 3.2
+        if teclas[pygame.K_RIGHT]:
+            x += 1 * velocidade
+        if teclas[pygame.K_LEFT]:
+            x -= 1  * velocidade
+        if teclas[pygame.K_UP]:
+            y -= 1 * velocidade
+        if teclas[pygame.K_DOWN]:
+            y += 1 * velocidade
             
-    elif alma.estado == 2 :
-        velocidade = 0
-        pygame.display.set_caption("Você está verde!")
-    else:
-        pygame.display.set_caption("Você se enche de DETERMINAÇÃO")
-        velocidade = 0
+        #Sim, o código está falho ainda. Só tem 1 ataque ai quando acaba fica todo fodido como você pode ver. Eventualmente conserto
+        if ataque.mostrar:
+            ataque.draw()
+            if ataque.contador == 3: #botei isso aqui pra caso o ataque seja resetado pro começo da tela 3 vezes ele parar o ataque e voltar pra tela inicial
+                janela.mudarTela('ações')
+                janela.atualizaTela()    
+            if ataque.atualizar(alma.rect) and ataque.mostrar == False:
+                vidaAtual -= 1
+                somColisao = pygame.mixer.Sound('assets/sounds/snd_hurt1.wav')
+                somColisao.set_volume(0.4)
+                somColisao.play()
+                janela.mudarTela('ações')
+                janela.atualizaTela()
+                x = 65
+                y = 450
 
-    artes.draw(janela.tela)
-    artes.update()
-    
     if vidaAtual <= 0:
         janela.mudarTela('gameover')
         tocouGameOver = False
