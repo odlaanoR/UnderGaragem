b61c22968bc49ea48bc72faa2c86b5ddc19aea82
Ronaldo Cesar <156006721+odlaanoR@users.noreply.github.com>
2025-07-15 11:29:54 -0300

    Update constantes.py

--- PCS/modulos/constantes.py
+++ PCS/modulos/constantes.py
@@ -2,8 +2,8 @@ import pygame
 pygame.init()
 
 #Definições Padrões
-largura = 1280
-altura = 720
+largura = 640
+altura = 480
 velocidade = 3.2
 x = 65
 y = 450
@@ -17,6 +17,11 @@ direcao = None
 #relogio de ataques
 ataque_iniciou = False
 fim_do_ataque = pygame.event.custom_type()
+fase_atual = 0
+
+#Contador de Ações
+usouConversar = 0
+usouVasculhar = 0
 
 #Fontes
 fonte = pygame.font.SysFont('arial', 30, True, False)
@@ -34,5 +39,8 @@ pygame.mixer.music.set_volume(0.45)
 pygame.mixer.music.play(-1)
 dano_snd = pygame.mixer.Sound('assets/sounds/dano.mp3')
 parry_snd = pygame.mixer.Sound('assets/sounds/parry.mp3')
+clica_som = pygame.mixer.Sound('assets/sounds/snd_select.mp3')
+cura_som = pygame.mixer.Sound('assets/sounds/snd_heal.wav')
+
 
 print('preparando constantes...')
