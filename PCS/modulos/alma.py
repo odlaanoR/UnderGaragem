cecfdcdd7d4505b2942b80b94bb253f82ed774d7
Ronaldo Cesar <156006721+odlaanoR@users.noreply.github.com>
2025-07-15 23:25:02 -0300

    Update alma.py

--- PCS/modulos/alma.py
+++ PCS/modulos/alma.py
@@ -2,7 +2,6 @@ import pygame
 from pygame.locals import *
 import modulos.constantes as cos
 
-
 pygame.init()
 
 class Alma(pygame.sprite.Sprite):
@@ -19,11 +18,28 @@ class Alma(pygame.sprite.Sprite):
         self.image = pygame.transform.scale(self.image, (16, 16))
         self.rect = self.image.get_rect()
         self.rect.center = cos.x, cos.y
+        self.acertavel = True
+        self.tempo_inicial = None
+        self.tempo_atual = None
+        self.gnomo = True
 
     def update(self):
         self.rect.center = cos.x, cos.y
         self.image = self.sprites[self.estado]
         self.image = pygame.transform.scale(self.image, (16, 16))
+
+    def iframe(self):
+        global fimInv, tempoInv
+        '''if self.gnomo:
+            self.tempo_inicial = pygame.time.get_ticks()
+            self.gnomo = False
+            
+        self.tempo_atual = pygame.time.get_ticks()
+        print(f'Tempo Atual: {self.tempo_atual}')
+        print(f'Tempo Inicial: {self.tempo_inicial}')
+        if self.tempo_inicial - self.tempo_atual <= cos.tempoInv:'''
+        self.acertavel = False
+        pygame.time.set_timer(cos.fimInv, cos.tempoInv, 1)
 
 alma = Alma()
 artes = pygame.sprite.Group()
