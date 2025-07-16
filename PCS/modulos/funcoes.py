c4b09bb0985e8380db6957fe7e2de660b73c5f06
Ronaldo Cesar <156006721+odlaanoR@users.noreply.github.com>
2025-07-15 23:25:47 -0300

    Update funcoes.py

--- PCS/modulos/funcoes.py
+++ PCS/modulos/funcoes.py
@@ -4,24 +4,26 @@ from modulos.janelas import janela
 from modulos.selecoes import *
 from modulos.item import *
 from modulos.alma import alma
-import modulos.ataques as atk
 import modulos.acoes as act
+import modulos.ataques as atk
+from modulos.ataques import *
 import modulos.constantes as cos
 
 escudo = pygame.draw.line(janela.tela, 'blue', (0,0), (0,0), 1)
-itemSelecionado = None
-consumiuItem = False
+
 #Essa função serve para reiniciar tudo que já aconteceu, então se, por exemplo, o jogador já tiver usado um item e ele morre na batalha, a função é chamada e restaura todos os itens dele.
-def reiniciar_jogo():
-    global vidaAtual, gameover, x, y, musicaFundo, itens, fase_atual, ataque_iniciou, fases, usouConversar, usouVasculhar, eventos
+def reiniciarJogo():
+    global vidaAtual, vida, gameover, x, y, musicaFundo, itens, fase_atual, ataque_iniciou, fases, eventos, usouConversar, usouVasculhar
     cos.vidaAtual = 70
+    cos.vida = 92
     cos.x = 65
     cos.y = 450
     gameover = False
-    cos.usouConversar = 0
-    cos.usouVasculhar = 0
+    eventos = pygame.event.get()
     cos.fase_atual = 0
     cos.ataque_iniciou = False
+    cos.usouConversar = 0
+    cos.usouVasculhar = 0
     musicaFundo = pygame.mixer.music.load('assets/sounds/Project147.mp3')
     pygame.mixer.music.set_volume(0.45)
     pygame.mixer.music.play(-1)
@@ -40,20 +42,21 @@ def reiniciar_jogo():
     print('reiniciando jogo')
     fases = [
         atk.Gerarataques(atk.rodada1(), 10),
-        atk.Gerarataques(atk.rodada2(), 10)
+        atk.Gerarataques(atk.rodada2(), 10),
+        atk.Gerarataques(atk.rodada3(), 10)
     ]
+    alma.acertavel = True
     for evento in eventos:
         for botao in botoes:      
             if evento.type == cos.fim_do_ataque and janela.telaAtual == 'lutaAcontecendo':#Encerra o turno de ataque inimigo
-                ataque.mostrar = False
+                ataque.mostrar = True
                 janela.mudarTela('seleções')
                 cos.x = 65
                 cos.y = 450
                 print("fim do ataque")
                 botao.impedeTravaPos = False
                 botao.comecaBatalha = False
-                cos.fase_atual += 1
-    
+                cos.fase_atual += 1  
 #Quando a função é chamada, digita todos os textos dos itens na tela (de forma organizada). Além de criar a colisão nos itens inscritos na tela (junto com o gambiarra (definido no módulo de seleções))   
 def printaItens():
     iy = 0
@@ -61,7 +64,7 @@ def printaItens():
     colisaoItens = []
     for item in itens:
         if iy <= 3:
-            posicaoItem = (170 + ix * 200, 205 + iy * 35)
+            posicaoItem = (175 + ix * 200, 215 + iy * 35)
             item.colisao = pygame.Rect(posicaoItem[0] - 20, posicaoItem[1] + 10, 50, 10) 
             colisaoItens.append(item.colisao)
             janela.escreveTexto(f'{item.nome}', cos.fonteBatalha, (255,255,255), posicaoItem)
@@ -71,20 +74,6 @@ def printaItens():
         else:
             iy = 0
             ix += 1
-
-#Se o jogador estiver na tela do inventário e confirmar a ação (clicando no Z) estando em colisao com qualquer item, ele será redirecionado para a tela que confirma qual item ele usou e o efeito que ele faz.
-def consomeItem(tecla):
-    global itemSelecionado, consumiuItem, mostraTransicao, transicaoTempo
-    if janela.telaAtual == 'inventário' and tecla == K_z:
-        for item in itens:
-            if alma.rect.colliderect(item.colisao):
-                itemSelecionado = item
-                janela.mudarTela('transiçãoItens')
-                transicaoTempo = pygame.time.get_ticks()
-                mostraTransicao = True
-                cos.clica_som.set_volume(0.4)
-                cos.clica_som.play() 
-                
 def printaAcoes():
     ix = 0
     iy = 0
@@ -105,6 +94,21 @@ def printaAcoes():
             janela.escreveTexto(f'{acao.nome}', cos.fonteBatalha, (255,255,255), posicaoAcao)
             iy += 1
             
+#Se o jogador estiver na tela do inventário e confirmar a ação (clicando no Z) estando em colisao com qualquer item, ele será redirecionado para a tela que confirma qual item ele usou e o efeito que ele faz.
+def consomeItem(tecla):
+    global itemSelecionado, consumiuItem, mostraTransicao, transicaoTempo
+    itemSelecionado = None
+    consumiuItem = False
+    if janela.telaAtual == 'inventário' and tecla == K_z:
+        for item in itens:
+            if alma.rect.colliderect(item.colisao):
+                itemSelecionado = item
+                janela.mudarTela('transiçãoItens')
+                transicaoTempo = pygame.time.get_ticks()
+                mostraTransicao = True
+                cos.clica_som.set_volume(0.4)
+                cos.clica_som.play()
+            
 def escolheAcao(tecla):
     global acaoSelecionada, usouAcao, mostraTransicao, transicaoTempo
     acaoSelecionada = None
@@ -121,21 +125,21 @@ def escolheAcao(tecla):
                 transicaoTempo = pygame.time.get_ticks()
                 mostraTransicao = True
                 cos.clica_som.set_volume(0.4)
-                cos.clica_som.play()
-
+                cos.clica_som.play()   
 def verde():
     global escudo
     if cos.direcao == "cima":
-        escudo = pygame.draw.line(janela.tela, 'green', ((janela.tela.get_width()/2)-50, (janela.tela.get_height()/2)-50), ((janela.tela.get_width()/2)+50, (janela.tela.get_height()/2)-50), 4)
+        escudo = pygame.draw.line(janela.tela, 'green', ((janela.tela.get_width()/2)-30, (janela.tela.get_height()/2)-30), ((janela.tela.get_width()/2)+30, (janela.tela.get_height()/2)-30), 4)
         return escudo
     elif cos.direcao == "baixo":
-        escudo = pygame.draw.line(janela.tela, 'yellow', ((janela.tela.get_width()/2)-50, (janela.tela.get_height()/2)+50), ((janela.tela.get_width()/2)+50, (janela.tela.get_height()/2)+50), 4)
+        escudo = pygame.draw.line(janela.tela, 'yellow', ((janela.tela.get_width()/2)-30, (janela.tela.get_height()/2)+30), ((janela.tela.get_width()/2)+30, (janela.tela.get_height()/2)+30), 4)
         return escudo
     elif cos.direcao == "esquerda":
-        escudo = pygame.draw.line(janela.tela, 'red', ((janela.tela.get_width()/2)-50, (janela.tela.get_height()/2)+50), ((janela.tela.get_width()/2)-50, (janela.tela.get_height()/2)-50), 4)
+        escudo = pygame.draw.line(janela.tela, 'red', ((janela.tela.get_width()/2)-30, (janela.tela.get_height()/2)+30), ((janela.tela.get_width()/2)-30, (janela.tela.get_height()/2)-30), 4)
         return escudo
     elif cos.direcao == "direita":
-        escudo = pygame.draw.line(janela.tela, 'blue', ((janela.tela.get_width()/2)+50, (janela.tela.get_height()/2)+50), ((janela.tela.get_width()/2)+50, (janela.tela.get_height()/2)-50), 4)
+        escudo = pygame.draw.line(janela.tela, 'blue', ((janela.tela.get_width()/2)+30, (janela.tela.get_height()/2)+30), ((janela.tela.get_width()/2)+30, (janela.tela.get_height()/2)-30), 4)
         return escudo
+    
                 
 print('inicializando funções...')
