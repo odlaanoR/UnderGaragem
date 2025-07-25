031d8844a4878801eb055b0e769e2ac9829aba17
Lucas Leandro <163912927+Lucas5812@users.noreply.github.com>
2025-07-24 22:38:04 -0300

    Novos dialogos e etc
    algumas novas variaveis e novos dialogos

--- PCS/modulos/constantes.py
+++ PCS/modulos/constantes.py
@@ -14,6 +14,7 @@ fps = pygame.time.Clock()
 vida = 92
 vidaAtual = 70
 direcao = None
+telainicial = True
 
 #Variaveis de ataques e etc
 ataque_iniciou = False
@@ -52,24 +53,24 @@ usouAcao = False
 
 #Fontes
 fonte = pygame.font.SysFont('arial', 30, True, False)
-fonteCustomizada = pygame.font.Font('assets/fonte2.ttf', 23)
+fonteCustomizada = pygame.font.Font('PCS/assets/fonte2.ttf', 23)
 fonteBatalha = pygame.font.SysFont('comicsans', 20, True, False)
 pygame.display.set_caption('Undergaragem')
 
 #Imagens
-gameoverImg = pygame.image.load('assets/sprites/gameover.png')
+gameoverImg = pygame.image.load('PCS/assets/sprites/gameover.png')
 gameoverImg = pygame.transform.scale(gameoverImg, (600, 300))
-icon = pygame.image.load('assets/sprites/alma.png')
+icon = pygame.image.load('PCS/assets/sprites/alma.png')
 pygame.display.set_icon(icon)
 
 #Sons/Músicas
-musicaFundo = pygame.mixer.music.load('assets/sounds/Project147.mp3')
+musicaFundo = pygame.mixer.music.load('PCS/assets/sounds/Project147.mp3')
 pygame.mixer.music.set_volume(0.1)
 pygame.mixer.music.play(-1)
-dano_snd = pygame.mixer.Sound('assets/sounds/dano.mp3')
-parry_snd = pygame.mixer.Sound('assets/sounds/parry.mp3')
-clica_som = pygame.mixer.Sound('assets/sounds/snd_select.mp3')
-cura_som = pygame.mixer.Sound('assets/sounds/snd_heal.wav')
+dano_snd = pygame.mixer.Sound('PCS/assets/sounds/dano.mp3')
+parry_snd = pygame.mixer.Sound('PCS/assets/sounds/parry.mp3')
+clica_som = pygame.mixer.Sound('PCS/assets/sounds/snd_select.mp3')
+cura_som = pygame.mixer.Sound('PCS/assets/sounds/snd_heal.wav')
 
 #textos
 dialogos = (
@@ -79,8 +80,15 @@ dialogos = (
     ("*Sr. Tremba mexeu no seu relógio", "o tempo parece se alterar levemente"),
     "C o n t i n u e  a t a c a n d o",
     "*O ar estrala de puro terror",
-    "*Estrelas caem ao seu redor",
-    "*Você escuta uma notificação de email ao longe"
+    "*Caminhões tombam ao seu redor",
+    "*Você escuta uma notificação de email ao longe",
+    "*Que lugar é esse....?",
+    ". . .",
+    "CONTINUE VASCULHANDO, DEVE TER ALGO AQUI",
+    "*Panelas batem em algum lugar a sua direita",
+    "*Você escuta uma moto cortando um giro em outra realidade",
+    "*ratos fazem barulho ao seu redor",
+    "*"
 )
 
 
