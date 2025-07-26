73ae8687f3e082b922ad340d3c4ea951dd851744
Ronaldo Cesar <156006721+odlaanoR@users.noreply.github.com>
2025-07-26 10:26:01 -0300

    Update item.py

--- PCS/modulos/item.py
+++ PCS/modulos/item.py
@@ -52,11 +52,6 @@ class Item():
             vidaAleatoria = randint(0,70)
             cos.vidaAtual = min(cos.vidaAtual + vidaAleatoria, cos.vida)
             print(f'Curou {vidaAleatoria} com A gororoba')
-        elif self.nome == 'O revólver que matou "Felipe"':
-            cos.jogador_atk += 998
-
-
-
         if self.quantidade > 0:
             self.quantidade -= 1
         if self.quantidade <= 0 and self in itens:
@@ -67,8 +62,6 @@ class Item():
         print(f'Atk: {cos.jogador_atk} | Def: {cos.jogador_def}')
         print(f'Itens restantes: {[item.nome for item in itens]}')
         '''
-
-
     def checaAlma(self):
         if alma.rect.colliderect(self.colisao):
             if self.passou:
@@ -79,17 +72,13 @@ class Item():
         else:
             self.passou = True
 
-            
 itens = [
     Item('Bolo de Sushi', 'Você recupera 40 de vida'),
     Item('Cuscuz Paulista','NAOOOOOOOOOOOOOOOOOOOOOOO'),
     Item('Sopa do Infinito','Você sente como se todas as coisas estivessem equilibradas'),
     Item('100 limite','Você se sente ilimitado (por uma rodada)'),
     Item('Verde','+10 HP Você está verde???????'),
-    Item('BiGaragem','Você recupera 50 de vida'),
-    Item('MacLanche Infeliz','De repente, tudo parece uma desgraça. + DEF, +ATK'),
-    Item('Gororoba Misteriosa','????????'),
-    Item('O revólver que matou "Felipe"', 'Você equipou essa coisa, você escuta gritos ao seu redor')
+    Item('Gororoba Misteriosa','Cura aleatória. Boa sorte!'),
 ]
 
 print('item carregando...')
