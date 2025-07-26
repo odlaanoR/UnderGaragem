9db647ba12b0fa2eaf2b26cf1871fcba7fcd0208
Ronaldo Cesar <156006721+odlaanoR@users.noreply.github.com>
2025-07-26 17:15:56 -0300

    Update constantes.py

--- PCS/modulos/constantes.py
+++ PCS/modulos/constantes.py
@@ -1,20 +1,24 @@
 import pygame
 
+
 pygame.init()
 
 #Definições Padrões
 largura = 640
 altura = 480
 velocidade = 3.2
-x = 65
-y = 450
+x = 286
+y = 270
 transicaoTempo = 0
 mostraTransicao = False
-fps = pygame.time.Clock()
+transicaoFinal = False
+#transicaoGenocida = False
+fps = pygame.time.Clock()   
 vida = 92
-vidaAtual = 70
+vidaAtual = 92
 direcao = None
 zerouJogo = False
+desapareceMensagem = False
 #telainicial = True
 
 #Tela do Menu
@@ -29,15 +33,14 @@ fase_atual = 0
 dialogo_atual = 0
 ataques_acabaram = False #só se torna True quando todos os ataques criados acabam
 tempoInv = 900
-
+velocidade_azul = 6.0
 jogador_def = 0
 jogador_atk = 1
 
 wilson_def = 99#LEMBRA DE MUDAR ISSO PELO AMOR DE DEUS
 wilson_atk = 10
 wilson_vida_max = 14000
-wilson_vida_atual = 14000
-
+wilson_vida_atual = 5
 
 caixa_combate = pygame.Rect(70, 190, 500, 180)
 
@@ -57,32 +60,34 @@ encontrouRevolver = False
 
 #Contador de Ações
 usouConversar = 0
-usouVasculhar = 0
+usouVasculhar = 2
 acaoSelecionada = None
 usouAcao = False
 
 #Fontes
 fonte = pygame.font.SysFont('arial', 30, True, False)
-fonteCustomizada = pygame.font.Font('PCS/assets/fonte2.ttf', 23)
+fonteCustomizada = pygame.font.Font('assets/fonte2.ttf', 23)
+fonteDano = pygame.font.Font('assets/hachicro.TTF', 35)
 fonteBatalha = pygame.font.SysFont('comicsans', 20, True, False)
 pygame.display.set_caption('Undergaragem')
 
 #Imagens
-gameoverImg = pygame.image.load('PCS/assets/sprites/gameover.png')
+gameoverImg = pygame.image.load('assets/sprites/gameover.png')
 gameoverImg = pygame.transform.scale(gameoverImg, (600, 300))
-icon = pygame.image.load('PCS/assets/sprites/alma.png')
+icon = pygame.image.load('assets/sprites/alma.png')
 pygame.display.set_icon(icon)
-
-WilsonIddle = pygame.image.load('PCS/assets/sprites/Wilsoniddle.png')
+WilsonIddle = pygame.image.load('assets/sprites/Wilsoniddle.png')
+tituloMenu = pygame.image.load('assets/sprites/UNDERGARAGEM.png')
+tituloMenu = pygame.transform.scale(tituloMenu, (560, 500))
 
 #Sons/Músicas
-musicaFundo = pygame.mixer.music.load('PCS/assets/sounds/Project147.mp3')
-pygame.mixer.music.set_volume(0.1)
+musicaFundo = pygame.mixer.music.load("assets/sounds/[Tremba's Contract].mp3")
+pygame.mixer.music.set_volume(0.35)
 pygame.mixer.music.play(-1)
-dano_snd = pygame.mixer.Sound('PCS/assets/sounds/dano.mp3')
-parry_snd = pygame.mixer.Sound('PCS/assets/sounds/parry.mp3')
-clica_som = pygame.mixer.Sound('PCS/assets/sounds/snd_select.mp3')
-cura_som = pygame.mixer.Sound('PCS/assets/sounds/snd_heal.wav')
+dano_snd = pygame.mixer.Sound('assets/sounds/dano.mp3')
+parry_snd = pygame.mixer.Sound('assets/sounds/parry.mp3')
+clica_som = pygame.mixer.Sound('assets/sounds/snd_select.mp3')
+cura_som = pygame.mixer.Sound('assets/sounds/snd_heal.wav')
 tocouMenu = False
 tocouFimPacifista = False
 
@@ -90,9 +95,9 @@ tocouFimPacifista = False
 dialogos = (
     "*Aqui acabou",
     "*Quem é esse cara?",
-    ("*diversas caixas e coisas estão próximas", "Use VASCULHAR para procurar por algo útil ao seu redor"),
-    ("*Wilson Tremba parece tão confuso quanto você", "talvez CONVERSAR possa torna-lo amistoso?"),
+    ("*Você olha ao redor, diversas caixas e coisas estão próximas", "Use VASCULHAR para procurar por algo útil"),
     ("*Sr. Tremba mexeu no seu relógio", "o tempo parece se alterar levemente"),
+    "C o n t i n u e  a t a c a n d o",
     "*O ar estrala de puro terror",
     "*Caminhões tombam ao seu redor",
     "*Você escuta uma notificação de email ao longe",
